caba331d8d461338bb5d387c29733474932ad307
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-08-18 05:13:14 +0700

    gratis satu bulan pertama

--- app/components/PricingComponent.js
+++ app/components/PricingComponent.js
@@ -59,7 +59,7 @@ export function PricingComponent() {
           <div key={i} className='flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white'>
           <h3 className='mb-4 text-2xl font-semibold'>{x?.name}</h3>
           <p className='font-light text-gray-500 sm:text-lg dark:text-gray-400'>
-            Best option for personal use & for your next project.
+            Gratis satu bulan pertama
           </p>
           <div className='flex justify-center items-baseline my-8'>
             <span className='mr-2 text-5xl font-extrabold'>IDR {priceFormat(x?.price)}</span>
