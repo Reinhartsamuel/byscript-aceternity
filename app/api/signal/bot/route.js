303b517a4dae9dc973de014ea35bdcab77d6f335
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-10-01 02:59:22 +0700

    change to lookup for every signal

--- app/api/signal/bot/route.js
+++ app/api/signal/bot/route.js
@@ -1,5 +1,6 @@
 import { adminDb } from '@/lib/firebase-admin-config';
 import { FieldValue } from 'firebase-admin/firestore';
+import moment from 'moment';
 
 const threeCommasUrl = 'https://app.3commas.io/trade_signal/trading_view';
 
@@ -32,6 +33,159 @@ const threeCommasUrl = 'https://app.3commas.io/trade_signal/trading_view';
 //   pair: '',
 // };
 
+// export async function POST(request) {
+//   try {
+//     const body = await request.json();
+//     const addWebhookResult = await adminDb.collection('webhooks').add({
+//       ...body,
+//       type: 'autotrade',
+//       createdAt: new Date(),
+//       // result: result.map((x) => x?.status),
+//     });
+//     // console.log(body);
+
+//     // trading_plan_id is constructed of trading plan name and pair
+//     const tp_unique_id = body?.trading_plan_id + '_' + body?.pair;
+
+//     // find bots id
+//     const doc = await adminDb
+//       .collection('trading_plan_pair')
+//       .doc(tp_unique_id)
+//       .get();
+      
+//     if (!doc.exists) {
+//       try {
+//         console.log(
+//           `No such document! id ::: ${
+//             body?.trading_plan_id || ''
+//           }, timestamp : `,
+//           new Date().getTime(),
+//           'creating',
+//           tp_unique_id
+//         );
+//         await adminDb.collection('trading_plan_pair').doc(tp_unique_id).set({
+//           bots_id: [],
+//           createdAt: new Date(),
+//           lastUpdated: new Date(),
+//           pair: body?.pair,
+//           trading_plan_id: body.trading_plan_id,
+//         });
+//         const tradingPlanDoc = await adminDb
+//           .collection('trading_plan')
+//           .doc(body.trading_plan_id)
+//           .get();
+
+//         if (!tradingPlanDoc.exists) {
+//           console.log(
+//             `trading plan not found, creating ID : ${body.trading_plan_id}`
+//           );
+//           await adminDb
+//             .collection('trading_plans')
+//             .doc(body.trading_plan_id)
+//             .set({
+//               id: body?.trading_plan_id || '',
+//               name: body?.trading_plan_id || '',
+//               childrenPairs: FieldValue.arrayUnion(body?.pair),
+//               createdAt: new Date(),
+//             });
+//         }
+
+//         return new Response('no bots!', {
+//           status: 400,
+//         });
+//       } catch (error) {
+//         console.log(error.message);
+//       }
+//     }
+//     const data = doc.data();
+//     const botsArray = data?.bots_id || [];
+
+//     if (botsArray?.length === 0) {
+//       console.log('no bots available, timestamp : ', new Date().getTime());
+//       return new Response('no bots!', {
+//         status: 400,
+//       });
+//     }
+
+//     const result = await Promise.allSettled(
+//       botsArray?.map(async (bot, i) => {
+//         const sendBodyTo3Commas = {
+//           message_type: 'bot',
+//           bot_id: parseInt(bot),
+//           email_token: body?.email_token,
+//           delay_seconds: body?.delay_seconds,
+//           pair: body?.pair,
+//         };
+//         if (body?.action) sendBodyTo3Commas.action = body?.action;
+//         const res = await fetch(threeCommasUrl, {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify(sendBodyTo3Commas),
+//         });
+//         if (parseInt(i) == 0)
+//           console.log(JSON.stringify(sendBodyTo3Commas), 'sendBodyTo3Commas');
+//         const returnValue = await res.text();
+//         return { ...returnValue, statusCode: res.status, sendBodyTo3Commas };
+//       })
+//     );
+
+//     if (Array.isArray(result) && result?.length > 0) {
+//       await Promise.allSettled(
+//         result?.map(async (x) => {
+//           let findBotOwner = { email: '', uid: '', name: '' };
+//           const botsRef = adminDb.collection('dca_bots');
+//           const snapshot = await botsRef
+//             .where(
+//               'bot_id',
+//               '==',
+//               x?.value?.sendBodyTo3Commas?.bot_id?.toString()
+//             )
+//             .get();
+//           if (!snapshot.empty) {
+//             snapshot.forEach((doc) => {
+//               findBotOwner.email = doc.data()?.email || '';
+//               findBotOwner.name = doc.data()?.name || '';
+//               findBotOwner.uid = doc.data()?.uid || '';
+//               findBotOwner.exchange_name = doc.data()?.exchange_name || '';
+//               findBotOwner.exchange_thumbnail =
+//                 doc.data()?.exchange_thumbnail || '';
+//               findBotOwner.autotraderCreatedAt = doc.data()?.createdAt || '';
+//             });
+//           }
+//           await adminDb.collection('3commas_logs').add({
+//             requestBody: JSON.stringify(body),
+//             createdAt: new Date(),
+//             response: x,
+//             autotradePostBody: x?.sendBodyTo3Commas || null,
+//             webhookId: addWebhookResult?.id || '',
+//             trading_plan_id: body?.trading_plan_id,
+//             pair: body?.pair || '',
+//             timeframe: body?.timeframe || '',
+//             timestamp: body?.timestamp || '',
+//             bot_id: x?.value?.sendBodyTo3Commas?.bot_id,
+//             type: 'autotrade',
+//             ...findBotOwner,
+//           });
+//         })
+//       );
+//     }
+
+//     // find if tradingplan already exists
+
+//     return new Response('ok', {
+//       status: 200,
+//     });
+//   } catch (error) {
+//     console.log(error.message, 'error autotrade');
+//     return new Response(error.message, {
+//       status: 400,
+//     });
+//   }
+// }
+
+
 export async function POST(request) {
   try {
     const body = await request.json();
@@ -46,7 +200,8 @@ export async function POST(request) {
     // trading_plan_id is constructed of trading plan name and pair
     const tp_unique_id = body?.trading_plan_id + '_' + body?.pair;
 
-    // find bots id
+    // check if trading plan pair exists on database
+    // if not, create
     const doc = await adminDb
       .collection('trading_plan_pair')
       .doc(tp_unique_id)
@@ -95,21 +250,31 @@ export async function POST(request) {
         console.log(error.message);
       }
     }
-    const data = doc.data();
-    const botsArray = data?.bots_id || [];
 
-    if (botsArray?.length === 0) {
-      console.log('no bots available, timestamp : ', new Date().getTime());
-      return new Response('no bots!', {
-        status: 400,
-      });
+    // lookup from dca_bots
+    const autotraderRef = adminDb.collection('dca_bots');
+    const snapshot = await autotraderRef
+      .where('trading_plan_pair', 'array-contains', tp_unique_id)
+      .where('status', '==', 'ACTIVE')
+      .get();
+    if (snapshot.empty) {
+      console.log(
+        `no bots found lookup under ${tp_unique_id} timestamp : `,
+        new Date().getTime()
+      );
+      return Response.json({ status: false, message: 'No bots foundd' });
     }
+    let autotraderLookups = [];
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+      autotraderLookups.push({ id: doc.id, ...doc.data() });
+    });
 
     const result = await Promise.allSettled(
-      botsArray?.map(async (bot, i) => {
+      autotraderLookups?.map(async (autotraderData) => {
         const sendBodyTo3Commas = {
           message_type: 'bot',
-          bot_id: parseInt(bot),
+          bot_id: parseInt(autotraderData.bot_id),
           email_token: body?.email_token,
           delay_seconds: body?.delay_seconds,
           pair: body?.pair,
@@ -122,55 +287,43 @@ export async function POST(request) {
           },
           body: JSON.stringify(sendBodyTo3Commas),
         });
-        if (parseInt(i) == 0)
-          console.log(JSON.stringify(sendBodyTo3Commas), 'sendBodyTo3Commas');
         const returnValue = await res.text();
-        return { ...returnValue, statusCode: res.status, sendBodyTo3Commas };
+        return {  ...autotraderData,...sendBodyTo3Commas, ...returnValue, statusCode: res.status, sendBodyTo3Commas };
       })
     );
+    // return Response.json({
+    //   status: 'okelah',
+    //   result,
+    // });
 
     if (Array.isArray(result) && result?.length > 0) {
       await Promise.allSettled(
         result?.map(async (x) => {
-          let findBotOwner = { email: '', uid: '', name: '' };
-          const botsRef = adminDb.collection('dca_bots');
-          const snapshot = await botsRef
-            .where(
-              'bot_id',
-              '==',
-              x?.value?.sendBodyTo3Commas?.bot_id?.toString()
-            )
-            .get();
-          if (!snapshot.empty) {
-            snapshot.forEach((doc) => {
-              findBotOwner.email = doc.data()?.email || '';
-              findBotOwner.name = doc.data()?.name || '';
-              findBotOwner.uid = doc.data()?.uid || '';
-              findBotOwner.exchange_name = doc.data()?.exchange_name || '';
-              findBotOwner.exchange_thumbnail =
-                doc.data()?.exchange_thumbnail || '';
-              findBotOwner.autotraderCreatedAt = doc.data()?.createdAt || '';
-            });
-          }
           await adminDb.collection('3commas_logs').add({
+            // from request body
             requestBody: JSON.stringify(body),
-            createdAt: new Date(),
-            response: x,
-            autotradePostBody: x?.sendBodyTo3Commas || null,
-            webhookId: addWebhookResult?.id || '',
             trading_plan_id: body?.trading_plan_id,
             pair: body?.pair || '',
             timeframe: body?.timeframe || '',
             timestamp: body?.timestamp || '',
-            bot_id: x?.value?.sendBodyTo3Commas?.bot_id,
+            createdAt: new Date(),
+            response: x,
+            autotradePostBody: x?.sendBodyTo3Commas || null,
+            webhookId: addWebhookResult?.id || '',
+            bot_id: x?.value?.sendBodyTo3Commas?.bot_id?.toString() || x?.value?.bot_id?.toString() || '',
             type: 'autotrade',
-            ...findBotOwner,
+            exchange_thumbnail: x?.value?.exchange_thumbnail || '',
+            exchange_name : x?.value?.exchange_name || '',
+            name: x?.value?.name || '',
+            email : x?.value?.email || '',
+            uid : x?.value?.uid || '',
+            autotrader_name :  x?.autotrader_name || moment.unix(x?.value?.createdAt?.seconds).format('YYYY-MM-DD') +
+              '-' +
+              x?.value?.createdAt?.seconds
           });
         })
       );
     }
-
-    // find if tradingplan already exists
 
     return new Response('ok', {
       status: 200,
